209094e4f8b08c6bb69a14fd06bdcdcd536f6327
haihan <haihanh2@163.com>
2022-06-10 18:22:14 +0800

    add

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,11 +1,11 @@
 import { defineStore } from 'pinia'
-import { constantRouterMap, asyncRouterMap, noAuthRouter } from '@router'
+import { asyncRouterMap, noAuthRouter } from '@router'
 
 const MockServerRoute = [
   {
     path: '/userList',
     children: [{
-      path: 'index',
+      path: '/userList/index',
     }]
   },
   {
@@ -14,35 +14,39 @@ const MockServerRoute = [
   }
 ]
 
-function routerFilter (feRouter, ServerRouter, res, fPath) {
+let walkLevel = 1
+function routerFilter (feRouter, ServerRouter, res) {
   for (const serverItem of ServerRouter) {
     for (const feItem of feRouter) {
       if (serverItem.path === feItem.path) {
         if (!serverItem.children || !serverItem.children.length) {
-          const { path, ...data } = feItem
-          res.push({
-            path: fPath ? fPath + '/' + path : path,
-            ...data
-          })
+          res.push(feItem)
         } else {
           const { children, ...data } = feItem
           const tempData = { ...data }
-          tempData.children = routerFilter(feItem.children, serverItem.children, [], feItem.path)
+          walkLevel++
+          tempData.children = routerFilter(feItem.children, serverItem.children, [])
           res.push(tempData)
         }
         break
       } else if (serverItem.path.includes(feItem.path)) {
-        const fakeChildPath = [{
-          path: serverItem.path.slice(feItem.path.length + 1)
-        }]
-        const { path, ...data } = feItem
-        const tempData = { ...data, path: feItem.path }
-        tempData.children = routerFilter(feItem.children, fakeChildPath, [])
-        res.push(tempData)
+        if (walkLevel > 1) {
+          res.push(feItem)
+        } else {
+          const fakeChildPath = [{
+            path: serverItem.path.slice(feItem.path.length + 1)
+          }]
+          const { path, ...data } = feItem
+          const tempData = { ...data, path: feItem.path }
+          walkLevel++
+          tempData.children = routerFilter(feItem.children, fakeChildPath, [])
+          res.push(tempData)
+        }
         break
       } else {}
     }
   }
+  walkLevel--
   return res
 }
 
@@ -67,7 +71,7 @@ export const authStore = defineStore('auth', {
           }
         }
         let apiRoutes = res.data.list;
-        let accessedRouters = routerFilter(asyncRouterMap, MockServerRoute, [])
+        let accessedRouters = routerFilter(asyncRouterMap, MockServerRoute, [], 0)
         this.routes = [...accessedRouters, noAuthRouter]
         this.menus = apiRoutes
         resolve()
